36af49a2d7c158313ddaf7e262d563610d98cd7f
hunterd <Derek_Hunter@otpp.com>
2019-01-26 20:24:47 -0500

    Updated scripts for the 18.04 Ubuntu images

--- _backup_VM_main.cmd
+++ _backup_VM_main.cmd
@@ -1,4 +1,4 @@
 call _backup_setVars
-call backup_vm_home VMware-Xubuntu-16.04-Main C:\A_VirtualMachines %VMware_Archive_Dir% %TEMP_ZIP_DIR%
+call backup_vm_home VMware-Xubuntu-18.04_Main C:\A_VirtualMachines %VMware_Archive_Dir% %TEMP_ZIP_DIR%
 
 pause
--- _backup_VM_other.cmd
+++ _backup_VM_other.cmd
@@ -1,4 +1,4 @@
 call _backup_setVars
-call backup_vm_home VMware-Xubuntu-14.04-Other C:\A_VirtualMachines %VMware_Archive_Dir% %TEMP_ZIP_DIR%
+call backup_vm_home VMware-Xubuntu-18.04_Test C:\A_VirtualMachines %VMware_Archive_Dir% %TEMP_ZIP_DIR%
 
 REM  pause
